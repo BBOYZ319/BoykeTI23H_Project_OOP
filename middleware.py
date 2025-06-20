f494f68175ac8c60a2813665b85cd18f88bbda98
BBOYZ319 <boyfantastik18@gmail.com>
2025-06-20 21:51:16 +0700

    Delete middleware.py

--- middleware.py
+++
@@ -1,7 +0,0 @@
-def auth_required(func):
-    def wrapper(user, *args, **kwargs):
-        if not user:
-            print("[SERVER] AKSES DITOLAK: User belum login.")
-            return
-        return func(user, *args, **kwargs)
-    return wrapper
